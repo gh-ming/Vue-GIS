c983fe001a24ef6b2a6f605404be7a94402614a0
ghming <gh_ming00@163.com>
2022-12-15 15:14:35 +0800

    first

---
+++ vue-project/notebook/get-set.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <input type="checkbox" v-model="myCheck">
+    </div>
+  </template>
+  
+  <script>
+  import { set } from 'vue'
+  export default {
+    data(){
+      return{
+        checked:false,
+      }
+    },
+    computed:{
+      myCheck:{
+        get(){
+          return this.checked
+        },
+        set(val){
+            console.log(val);
+            this.checked = val;
+        }
+      }
+    }
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ vue-project/notebook/计算属性.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <input type="text" v-model="msg">
+      <p>{{value}}</p>
+      <div>list中price的和:{{res}}</div>
+    </div>
+  </template>
+  
+  <script>
+  // 计算属性 本质就是对data中的值进行计算，减少模板中的逻辑
+  export default {
+    data(){
+      return{
+        msg:"vue",
+        list:[
+          {name:"html",price:30},
+          {name:"css",price:20},
+          {name:"vue",price:40}
+        ]
+      }
+    },
+    computed:{
+      value(){
+        return this.msg.split("").reverse().join("").toUpperCase()
+      },
+      res(val){
+        var res = 0;
+        this.list.forEach(item=>{
+          res+=item.price
+        })
+        return res
+      }
+  
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
