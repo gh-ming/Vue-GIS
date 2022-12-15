8e921776ae3ea2ed7a49a8b92e93362fa06fd5ae
ghming <gh_ming00@163.com>
2022-12-15 15:16:30 +0800

    all

---
+++ vue-project/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <p text-align="center">购物车</p>
+    <ul v-for="item of arr" :key="item.name">
+      <li>
+        <input type="checkbox" v-model="item.state">
+      {{item.name}}
+      </li>
+      <li>{{item.public}}</li>
+      <li>{{item.price}}</li>
+      <li>{{item.num}}</li>
+    </ul>
+    <h4><input type="checkbox" v-model="allstate">全选</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      arr: [
+        {
+          name: "算法导论",
+          public: "2006-9",
+          price: 85,
+          num: 1,
+          state:false
+        },
+        {
+          name: "Unix编程艺术",
+          public: "2006-2",
+          price: 59,
+          num: 2,
+          state:false
+        },
+        {
+          name: "编程珠玑",
+          public: "2006-9",
+          price: 65,
+          num: 3,
+          state:false
+        },
+      ],
+
+    }
+  },
+  computed:{
+    allstate:{
+      get(){
+        //遍历每一项，当每一项都满足条件时，为true
+        return this.arr.every(item=>item.state)
+      },
+      set(val){
+        console.log(val)
+        this.arr.forEach(item=>{
+          item.state = val
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container{
+  width: 700px;
+  height: 700px;
+  position: relative;
+}
+.container ul{
+  overflow: hidden;
+  margin-left: 20px;
+}
+.container li{
+  list-style: none;
+  float: left;
+  margin-top: 40px;
+  margin-left: 50px;
+}
+.container h4{
+  position: absolute;
+  right: 0px;
+}
+</style>
---
+++ vue-project/src/components/Musicitem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="item">
+    <img
+    src="http://p3.music.126.net/FoF5_ROj-jU37263LhInsw==/109951163203287436.jpg?param=200y200"
+    alt=""
+    />
+    <p>hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+*{margin: 0px;padding: 0px;}
+.item{
+    width: 200px;
+    border:1px solid #333
+}
+</style>
